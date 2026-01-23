f5de15897881cd969825e96db65e0a82a4c5f477
David Hovey <davidhovey18@gmail.com>
2026-01-22 21:09:02 -0600

    initial commit

---
+++ lua/autocommands.lua
@@ -0,0 +1,21 @@
+-- [[ Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'r',
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
