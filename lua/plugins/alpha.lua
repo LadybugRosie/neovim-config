29dd720ebd28c8507dd61e0b7d4f4c16fc206f3b
David Hovey <davidhovey18@gmail.com>
2026-01-24 23:46:41 -0600

    Refined startup screen

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -4,6 +4,7 @@ return {
     'nvim-mini/mini.icons',
     'nvim-lua/plenary.nvim',
   },
+
   config = function()
     local status_ok, alpha = pcall(require, 'alpha')
     if not status_ok then
@@ -40,16 +41,16 @@ return {
       '               ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠉⠙⠿⠾⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                     ',
     }
     dashboard.section.buttons.val = {
-      dashboard.button('f', '  Find file', ':Telescope find_files <CR>'),
+      dashboard.button('f', '␣  Find file', ':Telescope find_files <CR>'),
       dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
       dashboard.button('r', '  Recently used files', ':Telescope oldfiles <CR>'),
-      dashboard.button('t', '  Find text', ':Telescope live_grep <CR>'),
-      dashboard.button('c', '  Configuration', ':e ~/.config/nvim/init.vim<CR>'),
-      dashboard.button('q', '  Quit Neovim', ':qa<CR>'),
+      dashboard.button('t', '⏎  Find text', ':Telescope live_grep <CR>'),
+      dashboard.button('c', '  Configuration', ':e ~/.config/nvim/init.lua<CR>'),
+      dashboard.button('q', '⏻  Quit Neovim', ':qa<CR>'),
     }
 
     local function footer()
-      return "Don't Stop Until You are Proud..."
+      return os.date '%c'
     end
 
     dashboard.section.footer.val = footer()
